4bd7606151a0a5b5b7c04656521b397a7cbc44ef
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2025-01-29 17:31:01 +0600

    Create basic react app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8900,
+    hot: true,
+  },
+};

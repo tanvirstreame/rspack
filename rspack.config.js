641382905d0d6e7d3631fb2569b88e1bbd07b329
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2025-01-29 17:43:21 +0600

    Add rspack.config

---
+++ rspack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlRspackPlugin = require("@rspack/core").HtmlRspackPlugin;
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlRspackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8900,
+    hot: true,
+  },
+};
